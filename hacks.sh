cca4d099a6fdabbcaa08980274d64c2638ecbc5e
Blackteam <blackteam@InjectAndBankEngines>
2018-04-03 14:49:51 -0400

    Removed blanks

--- hacks.sh
+++ hacks.sh
@@ -6,40 +6,40 @@ sudo passwd Lori
 
 #$name = "Bird_Man"
 #adduser BirdMan --force-badname
-#usermod -aG sudo BirdMan 
+#usermod -aG sudo BirdMan
 passwd BirdMan
 
 #$name = LarryBird
 #adduser LarryBird --force-badname
-#usermod -aG sudo LarryBird 
+#usermod -aG sudo LarryBird
 passwd LarryBird
 
 #$name = "Bird_Man"
 #adduser BarryBird --force-badname
-#usermod -aG sudo BarryBird 
+#usermod -aG sudo BarryBird
 passwd BarryBird
 
 #adduser MaryBird --force-badname
-#usermod -aG sudo MaryBird 
+#usermod -aG sudo MaryBird
 passwd MaryBird
 
 #adduser GaryBird --force-badname
-#usermod -aG sudo GaryBird 
+#usermod -aG sudo GaryBird
 passwd GaryBird
 
 #adduser PerryBird --force-badname
-#usermod -aG sudo PerryBird 
+#usermod -aG sudo PerryBird
 passwd PerryBird
 
 #adduser BerryBird --force-badname
-#usermod -aG sudo BerryBird 
+#usermod -aG sudo BerryBird
 passwd BerryBird
 
 #adduser TerryBird --force-badname
-#usermod -aG sudo TerryBird 
+#usermod -aG sudo TerryBird
 passwd TerryBird
 
 #adduser QueryBird --force-badname
-#usermod -aG sudo QueryBird 
+#usermod -aG sudo QueryBird
 passwd QueryBird
 
